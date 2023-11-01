1410a153c99460dd5e55ee56f73a3eb3d80f520b
Gnurro <knawurzelkopp@hotmail.de>
2023-11-01 18:18:26 +0100

    Remove Meta llama inference backend

--- setup_llama2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python3 -m venv venv_llama2
-source venv_llama2/bin/activate
-pip3 install -r requirements.txt
-pip3 install -r requirements_llama2.txt
-
-mkdir llama2
-
-echo "Please run download.sh from https://github.com/facebookresearch/llama/tree/main in the llama2 directory to acquire the Llama-2 model weights and tokenizer."
