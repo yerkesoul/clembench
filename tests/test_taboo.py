f8618712baec70391a8c9fbff2a94965a58d3d85
Philipp Sadler <philipp.sadler@gmail.com>
2024-04-11 13:42:10 +0200

    [taboo] fixes #76: GM now appends first clue to guesser's prompt

--- tests/test_taboo.py
+++ tests/test_taboo.py
@@ -1,9 +1,49 @@
 import unittest
 
-from games.taboo.master import check_clue
+from backends import CustomResponseModel
+from games.taboo.master import check_clue, Taboo
 
 
 class TabooTestCase(unittest.TestCase):
+
+    def test_taboo_messages_alternate(self):
+        outer_self = self
+        experiment = {"name": "test",
+                      "max_turns": 3,
+                      "describer_initial_prompt": "<describer-prompt",
+                      "guesser_initial_prompt": "<guesser-prompt>"
+                      }
+        game = {
+            "game_id": 0,
+            "target_word": "test",
+            "related_word": ["assurance", "ensure", "quality"],
+            "target_word_stem": "test",
+            "related_word_stem": ["assur", "ensure", "quality"]
+        }
+
+        # describer = CustomResponseModel() # not possible now, but the way to do it, could provide lambda in construct
+        # describer.generate_response = lambda messages: "clue", "clue", "clue"
+
+        # guesser = CustomResponseModel() # not possible now, but the way to do it, could provide lambda in construct
+        # describer.generate_response = lambda messages: "guess", "guess", "guess"
+
+        players = [CustomResponseModel(), CustomResponseModel()]
+
+        class TestTaboo(Taboo):
+
+            def _on_before_turn(self, turn_idx: int):
+                # in fact this check could go into the DialogueMaster
+                for name, messages in self.messages_by_names.items():
+                    print("test", turn_idx, name, messages)
+                    if turn_idx > 0:
+                        is_same = messages[turn_idx]["role"] == messages[turn_idx - 1]["role"]
+                        outer_self.assertFalse(is_same,
+                                               "Consecutive roles in the messages history should not be the same")
+                super()._on_before_turn(turn_idx)
+
+        taboo = TestTaboo(experiment, players)
+        taboo.setup(**game)
+        taboo.play()
 
     def test_clue_check_issue9(self):
         errors = check_clue("A term that refers to the act of completing a task without professional help",
